5fa2650185f6d174cb29c071824e05c6a1eeace4
askend3d <ramazan01012005@gmail.com>
2024-04-15 19:53:33 +0300

    asset update

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,25 +1,38 @@
-import { Select, Space, Typography, Flex, Divider, Form, InputNumber, Checkbox, Button } from "antd";
+import {
+    Select,
+    Space,
+    Typography,
+    Flex,
+    Divider,
+    Form,
+    InputNumber,
+    Checkbox,
+    Button,
+    DatePicker,
+} from "antd";
 import { useState } from "react";
 import { useCrypto } from "../context/cryptoContext";
 
+const validateMessages = {
+    required: "${label} is required!",
+    types: {
+        number: "${label} is not a number!",
+    },
+    number: {
+        range: "${label} must be between ${min} and ${max}",
+    },
+};
+
 function AddAssetForm() {
-  const [coin, setCoin] = useState(null);
-  const { crypto } = useCrypto();
-  
-  const onFinish = (values) => {
-    console.log('Success:', values);
-  };
-
-
-  if (!coin) {
+    const [ form ] = Form.useForm();
     const [coin, setCoin] = useState(null);
     const { crypto } = useCrypto();
+
     if (!coin) {
         return (
             <Select
                 style={{ width: "100%" }}
                 onSelect={(v) => setCoin(crypto.find((c) => c.id === v))}
-                onClick={() => setSelect((prev) => !prev)}
                 placeholder="Select coin"
                 options={crypto.map((coin) => ({
                     label: coin.name,
@@ -35,99 +48,94 @@ function AddAssetForm() {
                         />{" "}
                         {option.data.label}
                     </Space>
-                )} 
+                )}
             />
         );
     }
+
+    function handleAmountChange(value) {
+        const price = form.getFieldValue("price");
+
+
+        form.setFieldsValue({
+            total: +(value * price).toFixed(2),
+        
+        })
+    }
+    function handlePriceChange(value) {
+        const amount = form.getFieldValue("amount");
+
+        form.setFieldsValue({
+            total: +(amount * value).toFixed(2),
+        
+        })
+    }
+
     return (
-      <Select
-        style={{ width: "100%" }}
-        onSelect={(v) => setCoin(crypto.find((c) => c.id === v))}
-        placeholder="Select coin"
-        options={crypto.map((coin) => ({
-          label: coin.name,
-          value: coin.id,
-          icon: coin.icon,
-        }))}
-        optionRender={(option) => (
-          <Space>
-            <img
-              style={{ width: 24 }}
-              src={option.data.icon}
-              alt={option.data.label}
-            />{" "}
-            {option.data.label}
-          </Space>
-        )}
-      />
+        <Form
+            name="basic"
+            form={form}
+            labelCol={{ span: 4 }}
+            wrapperCol={{ span: 10 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{
+                price: +coin.price.toFixed(2),
+            }}
+        >
+            <Flex align="center">
+                <img
+                    src={coin.icon}
+                    alt={coin.name}
+                    style={{ width: 32, marginRight: 8 }}
+                />
+                <Typography.Title style={{ margin: 0 }} level={2}>
+                    {coin.name}
+                </Typography.Title>
+            </Flex>
+            <Divider />
+            <Form.Item
+                label="Amount"
+                name="amount"
+                rules={[
+                    {
+                        required: true,
+                        type: "number",
+                        min: 0,
+                    },
+                ]}
+            >
+                <InputNumber
+                    placeholder="Enter coin amount"
+                    onChange={ handleAmountChange }
+                    style={{ width: "100%" }}
+                />
+            </Form.Item>
+
+            <Form.Item label="Price" name="price">
+                <InputNumber onChange={handlePriceChange} style={{ width: "100%" }} />
+            </Form.Item>
+            <Form.Item label="Date & Time" name="date">
+                <DatePicker showTime />
+            </Form.Item>
+            <Form.Item label="Total" name="total">
+                <InputNumber disabled style={{ width: "100%" }} />
+            </Form.Item>
+
+            <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{ offset: 8, span: 16 }}
+            >
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
     );
-  }
-
-  return (
-    <Form
-      name="basic"
-      labelCol={{ span: 4 }}
-      wrapperCol={{ span: 10 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{  }}
-      onFinish={onFinish}
-    >
-      <Flex align="center">
-        <img
-          src={coin.icon}
-          alt={coin.name}
-          style={{ width: 32, marginRight: 8 }}
-        />
-        <Typography.Title style={{ margin: 0 }} level={2}>
-          {coin.name}
-        </Typography.Title>
-      </Flex>
-      <Divider />
-      <Form.Item
-        label="Amount" 
-        name="amount"
-        rules={[{ required: true, 
-          type:'number',
-          min: 0,
-          message: 'Please input your username!' }]}
-      >
-        <InputNumber style={{width: '100%'}} />
-      </Form.Item>
-
-      <Form.Item
-        label="Price"
-        name="price"
-      >
-        <InputNumber disabled style={{width: '100%'}} />
-      </Form.Item>
-      <Form.Item
-        label="Date & Time"
-        name="date"
-      >
-        <InputNumber disabled style={{width: '100%'}} />
-      </Form.Item>
-      <Form.Item
-        label="Total"
-        name="total"
-      >
-        <InputNumber disabled style={{width: '100%'}} />
-      </Form.Item>
-
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  );
 }
 
 export default AddAssetForm;
